3f8988f1dd4c6ff4c04250213df0bfd496483bcb
Jaouad-am <32226336+Jaouad-am@users.noreply.github.com>
2019-06-30 17:25:19 +0100

    Add Position Code

--- PersonelTracking/FrmPosition.cs
+++ PersonelTracking/FrmPosition.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BLL;
+using DAL;
 
 namespace PersonelTracking
 {
@@ -20,6 +22,37 @@ namespace PersonelTracking
         private void button1_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+		List<DEPARTMENT> DepartmentList = new List<DEPARTMENT>(); 
+        private void FrmPosition_Load(object sender, EventArgs e)
+        {
+            DepartmentList = DepartmentBLL.GetDepartments();
+            cmbDepartment.DataSource = DepartmentList;
+            cmbDepartment.DisplayMember = "DepartmentName";
+            cmbDepartment.ValueMember = "ID";
+            cmbDepartment.SelectedIndex = -1;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (txtPosition.Text.Trim() == "")
+            {
+                MessageBox.Show("Please fill the name field");
+            }
+            else if (cmbDepartment.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please Select a Department !");
+            }
+            else
+            {
+                POSITION position = new POSITION();
+                position.PositionName = txtPosition.Text;
+                position.DepartmentID = Convert.ToInt32(cmbDepartment.SelectedValue);
+                PositionBLL.AddPosition(position);
+                MessageBox.Show("Position Added!");
+                txtPosition.Clear();
+                cmbDepartment.SelectedIndex = -1;
+            }
         }
     }
 }
