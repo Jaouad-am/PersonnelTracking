4fb65c087ed7034985432da3589b977576bb99a9
Jaouad-am <32226336+Jaouad-am@users.noreply.github.com>
2019-07-26 20:31:01 +0100

    Show Employee List Part 2

--- DAL/DAO/EmployeeDAO.cs
+++ DAL/DAO/EmployeeDAO.cs
@@ -21,6 +21,55 @@ namespace DAL.DAO
                 throw ex;
             }
         }
+		public static List<EmployeeDetailDTO> GetEmployees()
+        {
+            List<EmployeeDetailDTO> employeeList = new List<EmployeeDetailDTO>();
+            var list = (from e in db.EMPLOYEEs
+                        join d in db.DEPARTMENTs on e.DepartmentID equals d.ID
+                        join p in db.POSITIONs on e.PositionID equals p.ID
+                        select new
+                        {
+                            UserNo = e.UserNo,
+                            Name = e.Name,
+                            Surname = e.Surname,
+                            EmployeeID = e.ID,
+                            Password = e.Password,
+                            DepartmentName = d.DepartmentName,
+                            PositionName = p.PositionName,
+                            DepartmentID = e.DepartmentID,
+                            PositionID = e.PositionID,
+                            isAdmin = e.isAdmin,
+                            Salary = e.Salary,
+                            ImagePath = e.ImagePath,
+                            birthDay = e.BirthDay,
+                            address = e.Address
+
+                        }).OrderBy(x=>x.UserNo).ToList();
+
+            foreach (var item in list)
+            {
+                EmployeeDetailDTO dto = new EmployeeDetailDTO();
+                dto.Name = item.Name;
+                dto.UserNo = item.UserNo;
+                dto.Surname = item.Surname;
+                dto.EmployeeID = item.EmployeeID;
+                dto.Password = item.Password;
+                dto.DepartmentID = item.DepartmentID;
+                dto.DepartmentName = item.DepartmentName;
+                dto.PositionID = item.PositionID;
+                dto.PositionName = item.PositionName;
+                dto.isAdmin = item.isAdmin;
+                dto.Salary = item.Salary;
+                dto.Bhirthday = item.birthDay;
+                dto.Address = item.address;
+                employeeList.Add(dto);
+            }
+
+
+
+
+            return employeeList;
+        }
 
         public static List<EMPLOYEE> GetUsers(int v)
         {
--- PersonelTracking/FrmEmployeeList.cs
+++ PersonelTracking/FrmEmployeeList.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BLL;
+using DAL;
+using DAL.DTO;
 
 namespace PersonelTracking
 {
@@ -43,11 +46,36 @@ namespace PersonelTracking
             this.Visible = true;
         }
 		EmployeeDTO dto = new EmployeeDTO();
+        private bool combofull = false;
 
         private void FrmEmployeeList_Load(object sender, EventArgs e)
         {
             dto = EmployeeBLL.GetAll();
-            
+            dataGridView1.DataSource = dto.Employees;
+            dataGridView1.Columns[0].Visible = false;
+            dataGridView1.Columns[1].HeaderText = "UserNo";
+            dataGridView1.Columns[2].HeaderText = "Name";
+            dataGridView1.Columns[3].HeaderText = "Surname";
+            dataGridView1.Columns[4].HeaderText = "Department";
+            dataGridView1.Columns[5].HeaderText = "Position";
+            dataGridView1.Columns[6].Visible = false;
+            dataGridView1.Columns[7].Visible = false;
+            dataGridView1.Columns[8].HeaderText = "Salary";
+            dataGridView1.Columns[9].Visible = false;
+            dataGridView1.Columns[10].Visible = false;
+            dataGridView1.Columns[11].Visible = false;
+            dataGridView1.Columns[12].Visible = false;
+            dataGridView1.Columns[13].Visible = false;
+            combofull = false;
+            cmbDepartment.DataSource = dto.Departments;
+            cmbDepartment.DisplayMember = "DepartmentName";
+            cmbDepartment.ValueMember = "ID";
+            cmbPosition.DataSource = dto.Positions;
+            cmbPosition.DisplayMember = "PositionName";
+            cmbPosition.ValueMember = "ID";
+            cmbDepartment.SelectedIndex = -1;
+            cmbPosition.SelectedIndex = -1;
+            combofull = true;
 
 
 
