99f938e71217cff8ddc273e24e3bd7ca03467441
Jaouad-am <32226336+Jaouad-am@users.noreply.github.com>
2019-08-12 04:25:12 +0100

    Task Listing Part 2

--- PersonelTracking/FrmTaskList.cs
+++ PersonelTracking/FrmTaskList.cs
@@ -36,9 +36,40 @@ namespace PersonelTracking
         }
 
         TaskDTO dto = new TaskDTO();
+		private bool combofull = false;
         private void FrmTaskList_Load(object sender, EventArgs e)
         {
-            dto = TaskBLL.GetAll();
+			dto = TaskBLL.GetAll();
+            dataGridView1.DataSource = dto.Tasks;
+            dataGridView1.Columns[0].HeaderText = "Task Title";
+            dataGridView1.Columns[1].HeaderText = "UserNo";
+            dataGridView1.Columns[2].HeaderText = "Name";
+            dataGridView1.Columns[3].HeaderText = "Surname";
+            dataGridView1.Columns[4].HeaderText = "Start Date";
+            dataGridView1.Columns[5].HeaderText = "Delivery Date";
+            dataGridView1.Columns[6].HeaderText = "Task State";
+            dataGridView1.Columns[7].Visible = false;
+            dataGridView1.Columns[8].Visible = false;
+            dataGridView1.Columns[9].Visible = false;
+            dataGridView1.Columns[10].Visible = false;
+            dataGridView1.Columns[11].Visible = false;
+            dataGridView1.Columns[12].Visible = false;
+            dataGridView1.Columns[13].Visible = false;
+            dataGridView1.Columns[14].Visible = false;
+            combofull = false;
+            cmbDepartment.DataSource = dto.Departments;
+            cmbDepartment.DisplayMember = "DepartmentName";
+            cmbDepartment.ValueMember = "ID";
+            cmbPosition.DataSource = dto.Positions;
+            cmbPosition.DisplayMember = "PositionName";
+            cmbPosition.ValueMember = "ID";
+            cmbDepartment.SelectedIndex = -1;
+            cmbPosition.SelectedIndex = -1;
+            combofull = true;
+            cmbTaskState.DataSource = dto.TaskStates;
+            cmbTaskState.DisplayMember = "StateName";
+            cmbTaskState.ValueMember = "ID";
+            cmbTaskState.SelectedIndex = -1;
         }
 
         private void btnNew_Click(object sender, EventArgs e)
@@ -55,6 +86,15 @@ namespace PersonelTracking
             this.Hide();
             ts.ShowDialog();
             this.Visible = true;
+        }
+		private void cmbDepartment_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (combofull)
+            {
+                cmbPosition.DataSource = dto.Positions.Where(x => x.DepartmentID ==
+                Convert.ToInt32(cmbDepartment.SelectedValue)).ToList();
+
+            }
         }
     }
 }
