437497b61d16afbf34e95837d85d760b4c972dd3
Jaouad-am <32226336+Jaouad-am@users.noreply.github.com>
2019-10-01 23:11:03 +0100

    Permission List Part 3

--- PersonelTracking/FrmPermissionList.cs
+++ PersonelTracking/FrmPermissionList.cs
@@ -56,9 +56,45 @@ namespace PersonelTracking
             this.Visible = true;
         }
 		PermissionDTO dto = new PermissionDTO();
-        private void FrmPermissionList_Load(object sender, EventArgs e)
+		private bool combofull=false;
+        void FillAllData()
         {
             dto = PermissionBLL.GetAll();
+            dataGridView1.DataSource = dto.Permissions;
+            combofull = false;
+            cmbDepartment.DataSource = dto.Departments;
+            cmbDepartment.DisplayMember = "DepartmentName";
+            cmbDepartment.ValueMember = "ID";
+            cmbPosition.DataSource = dto.Positions;
+            cmbPosition.DisplayMember = "PositionName";
+            cmbPosition.ValueMember = "ID";
+            cmbDepartment.SelectedIndex = -1;
+            cmbPosition.SelectedIndex = -1;
+            combofull = true;
+            cmbState.DataSource = dto.States;
+            cmbState.DisplayMember = "StateName";
+            cmbState.ValueMember = "ID";
+            cmbState.SelectedIndex = -1;
+        }
+
+        private void FrmPermissionList_Load(object sender, EventArgs e)
+        {
+            FillAllData();
+            dataGridView1.Columns[0].Visible = false;
+            dataGridView1.Columns[1].HeaderText = "UserNo";
+            dataGridView1.Columns[2].HeaderText = "Name";
+            dataGridView1.Columns[3].HeaderText = "Surname";
+            dataGridView1.Columns[4].Visible = false;
+            dataGridView1.Columns[5].Visible = false;
+            dataGridView1.Columns[6].Visible = false;
+            dataGridView1.Columns[7].Visible = false;
+            dataGridView1.Columns[8].HeaderText = "Start Date";
+            dataGridView1.Columns[9].HeaderText = "End Date";
+            dataGridView1.Columns[10].Visible = false;
+            dataGridView1.Columns[11].HeaderText = "Day Amount";
+            dataGridView1.Columns[12].HeaderText = "State";
+            dataGridView1.Columns[13].Visible = false;
+            
         }
     }
 }
